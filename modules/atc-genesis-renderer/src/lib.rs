f31d1ed51449d18a97ff588475fca086886c73b6
ShivaCoreDev <michael.worob@gmail.com>
2026-09-16 19:13:32 +0200

    feat(renderer): expose deterministic weather system

--- modules/atc-genesis-renderer/src/lib.rs
+++ modules/atc-genesis-renderer/src/lib.rs
@@ -15,6 +15,7 @@ pub mod pipeline;
 pub mod render_graph;
 pub mod render_item;
 pub mod resources;
+pub mod weather;
 pub use animation::SkinnedPose;
 pub use backend::{BackendRenderer, CommandBufferBackend, GraphicsBackend, NullBackend, RenderBackend, RenderCapabilities};
 pub use batching::{build_batches, RenderBatch, RenderItem};
@@ -30,6 +31,7 @@ pub use pipeline::{command_error_is_recoverable, command_kind, DrawPacket, Graph
 pub use render_graph::{RenderGraphError, RenderPassDesc, RenderPassGraph, RenderPassId, RenderResourceId, ResourceAccess, ResourceUse};
 pub use render_item::{collect_entity, collect_render_items};
 pub use resources::{MaterialHandle, MeshHandle, MeshMaterialBinding, RenderResourceBindings, TextureHandle};
+pub use weather::{WeatherCell, WeatherClass, WeatherConfig, WeatherError, WeatherField};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Camera { pub position:[f32;3], pub forward:[f32;3], pub fov_y_radians:f32, pub near:f32, pub far:f32 }
