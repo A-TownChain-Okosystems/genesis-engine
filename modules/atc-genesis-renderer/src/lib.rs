dfcb94d5c6977d69284bbe9d83754bbd08dededb
ShivaCoreDev <michael.worob@gmail.com>
2026-09-16 18:37:28 +0200

    feat(renderer): expose native pipeline contracts

--- modules/atc-genesis-renderer/src/lib.rs
+++ modules/atc-genesis-renderer/src/lib.rs
@@ -5,6 +5,7 @@ pub mod batching;
 pub mod commands;
 pub mod ecs;
 pub mod materials;
+pub mod pipeline;
 pub mod render_item;
 pub mod resources;
 pub use animation::SkinnedPose;
@@ -12,6 +13,7 @@ pub use backend::{BackendRenderer, CommandBufferBackend, GraphicsBackend, NullBa
 pub use batching::{build_batches, RenderBatch, RenderItem};
 pub use commands::{RenderCommand, RenderCommandError, RenderCommandStream};
 pub use materials::{MaterialError, MaterialId, MaterialInstance, PbrMaterial};
+pub use pipeline::{command_error_is_recoverable, command_kind, DrawPacket, GraphicsPipelineDesc, PipelineError, PipelineId, PrimitiveTopology, RenderPipelineRegistry, ShaderId, ShaderModule, ShaderStage};
 pub use render_item::{collect_entity, collect_render_items};
 pub use resources::{MaterialHandle, MeshHandle, MeshMaterialBinding, RenderResourceBindings, TextureHandle};
 
