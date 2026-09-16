dce01005ccb2d72a8e2b194da60d0e51d732ec2a
ShivaCoreDev <michael.worob@gmail.com>
2026-09-16 21:10:59 +0200

    feat(gameplay): make production storage checks atomic

--- modules/atc-genesis-gameplay/src/production_execution.rs
+++ modules/atc-genesis-gameplay/src/production_execution.rs
@@ -3,7 +3,7 @@
 use crate::{CraftingJob, CraftingRecipe, Inventory, ProductionStation, ToolState};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
-pub enum ProductionFailure { InvalidRecipe, MissingIngredient, WrongStation, StationCapacity, MissingTechnology, MissingTool, ToolBroken, OutputBlocked, AlreadyCompleted }
+pub enum ProductionFailure { InvalidRecipe, MissingIngredient, WrongStation, StationCapacity, MissingTechnology, MissingTool, ToolBroken, InputStorageBlocked, OutputBlocked, AlreadyCompleted }
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct ProductionToolRequirement { pub tool_id: &'static str }
@@ -37,6 +37,9 @@ pub fn start_production(
         (None, Some(t)) if t.broken() => return Err(ProductionFailure::ToolBroken),
         _ => {}
     }
+    for ingredient in recipe.ingredients {
+        if !station.input_has_storage(ingredient.item_id, ingredient.quantity, u32::MAX) { return Err(ProductionFailure::InputStorageBlocked); }
+    }
     let job = CraftingJob::new(recipe).ok_or(ProductionFailure::InvalidRecipe)?;
     if !station.reserve_job() { return Err(ProductionFailure::StationCapacity); }
     let tool_id = tool.as_ref().map(|t| t.tool_id.to_string());
@@ -53,6 +56,7 @@ pub fn start_production(
 pub fn finish_production(job: &mut ProductionJob, station: &mut ProductionStation) -> Result<(), ProductionFailure> {
     if job.output_committed { return Err(ProductionFailure::AlreadyCompleted); }
     if job.station_id != station.id || !job.job.complete() || !station.enabled { return Err(ProductionFailure::OutputBlocked); }
+    if !station.output_has_storage(&job.output_item_id, job.output_quantity, u32::MAX) { return Err(ProductionFailure::OutputBlocked); }
     let remaining = station.store_output(&job.output_item_id, job.output_quantity, u32::MAX);
     if remaining != 0 { return Err(ProductionFailure::OutputBlocked); }
     job.output_committed = true;
@@ -68,7 +72,8 @@ mod tests {
     const RECIPE: CraftingRecipe = CraftingRecipe { id:"test", ingredients:ING, output_item_id:"plank", output_quantity:1, station:Some("workbench"), required_technology:Some("construction"), duration_ticks:2 };
     #[test] fn validation_failure_does_not_mutate() { let mut i=Inventory::default(); i.add("wood",2,10); let mut s=ProductionStation::new("workbench",ProductionStationKind::Workbench,1).unwrap(); assert_eq!(start_production(&RECIPE,&mut i,&mut s,&[],None,None),Err(ProductionFailure::MissingTechnology)); assert_eq!(i.count("wood"),2); assert_eq!(s.active_jobs,0); }
     #[test] fn capacity_blocks_second_job() { let mut i=Inventory::default(); i.add("wood",4,10); let mut s=ProductionStation::new("workbench",ProductionStationKind::Workbench,1).unwrap(); assert!(start_production(&RECIPE,&mut i,&mut s,&["construction"],None,None).is_ok()); assert_eq!(start_production(&RECIPE,&mut i,&mut s,&["construction"],None,None),Err(ProductionFailure::StationCapacity)); }
-    #[test] fn production_consumes_and_finishes_once() { let mut i=Inventory::default(); i.add("wood",2,10); let mut s=ProductionStation::new("workbench",ProductionStationKind::Workbench,1).unwrap(); let mut j=start_production(&RECIPE,&mut i,&mut s,&["construction"],None,None).unwrap(); assert_eq!(i.count("wood"),0); j.job.tick(2); assert!(finish_production(&mut j,&mut s).is_ok()); assert_eq!(s.output.count("plank"),1); assert_eq!(s.active_jobs,0); assert_eq!(finish_production(&mut j,&mut s),Err(ProductionFailure::AlreadyCompleted)); assert_eq!(s.output.count("plank"),1); }
+    #[test] fn production_consumes_and_finishes_once() { let mut i=Inventory::default(); i.add("wood",2,10); let mut s=ProductionStation::new("workbench",ProductionStationKind::Workbench,1).unwrap(); let mut j=start_production(&RECIPE,&mut i,&mut s,&["construction"],None,None).unwrap(); assert_eq!(i.count("wood"),0); j.job.tick(2); assert!(finish_production(&mut j,&mut s).is_ok()); assert_eq!(s.output.count("plank"),1); assert_eq!(s.active_jobs,0); assert_eq!(finish_production(&mut j,&mut s),Err(ProductionFailure::AlreadyCompleted)); }
+    #[test] fn output_capacity_blocks_without_job_release_or_mutation() { let mut i=Inventory::default(); i.add("wood",2,10); let mut s=ProductionStation::with_storage("workbench",ProductionStationKind::Workbench,1,1,1).unwrap(); s.output.add("plank",u32::MAX, u32::MAX); let mut j=start_production(&RECIPE,&mut i,&mut s,&["construction"],None,None).unwrap(); j.job.tick(2); assert_eq!(finish_production(&mut j,&mut s),Err(ProductionFailure::OutputBlocked)); assert!(!j.output_committed); assert_eq!(s.active_jobs,1); }
     #[test] fn required_tool_is_enforced_and_worn_once() { let mut i=Inventory::default(); i.add("wood",2,10); let mut s=ProductionStation::new("workbench",ProductionStationKind::Workbench,1).unwrap(); let mut t=ToolState::new("hammer",2).unwrap(); let req=ProductionToolRequirement{tool_id:"hammer"}; assert!(start_production(&RECIPE,&mut i,&mut s,&["construction"],Some(req),Some(&mut t)).is_ok()); assert_eq!(t.durability,1); }
     #[test] fn wrong_tool_does_not_mutate() { let mut i=Inventory::default(); i.add("wood",2,10); let mut s=ProductionStation::new("workbench",ProductionStationKind::Workbench,1).unwrap(); let mut t=ToolState::new("axe",2).unwrap(); let req=ProductionToolRequirement{tool_id:"hammer"}; assert_eq!(start_production(&RECIPE,&mut i,&mut s,&["construction"],Some(req),Some(&mut t)),Err(ProductionFailure::MissingTool)); assert_eq!(i.count("wood"),2); assert_eq!(t.durability,2); assert_eq!(s.active_jobs,0); }
 }
